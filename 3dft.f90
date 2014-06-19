9051bd27aac9107914f3373f3f81acdff9fbf0ef
refreshx2 <jjmaldonis@gmail.com>
2014-06-19 15:59:49 -0500

    Fixed the bug where pixel 127 or 128 was getting added twice.

--- 3dft.f90
+++ 3dft.f90
@@ -36,16 +36,15 @@ program ft3d
     !call read_model("alsm_New8C0.xyz", m, istat)
     !call read_model("al_3x3x3.xyz", m, istat)
     !call read_model("al_chunk.xyz", m, istat)
-    !call read_model("Zr50Cu35Al15_t3_final.xyz", m, istat)
+    call read_model("Zr50Cu35Al15_t3_final.xyz", m, istat)
     !call read_model("ZCA_t3_xtal.xyz", m, istat)
     !call read_model("xtal.t3.opposite.xyz", m, istat)
     !call read_model("icolike.t3.xyz", m, istat)
     !call read_model("mixed.xyz", m, istat)
-    call read_model("mixed_icolike.xyz", m, istat)
+    !call read_model("mixed_icolike.xyz", m, istat)
     call read_f_e
 
     allbinsize = 256
-    !allstart = -(28.28427/2.0)
     allstart = -1.5
 
     kminx = allstart
@@ -69,21 +68,10 @@ program ft3d
     write(*,*) "    kz: start:",kminz, "step:", dkz
 
 
-    !allocate(kgrid(nkx,nky,nkz))
     allocate(skgrid(nkx,nky,nkz))
     allocate(ikgrid(nkx,nky,nkz))
-    !kgrid = 0.0
     skgrid = (0.0,0.0)
     ikgrid = 0.0
-
-    ! Array lookup for k will be faster than calculating it every time
-    !do i=1, nkx
-    !    do j=1, nky
-    !        do k=1, nkz
-    !            kgrid(i,j,k) = sqrt((kminx+i*dkx)**2+(kminy+j*dky)**2+(kminz+k*dkz)**2) ! Make sure this is right
-    !        enddo
-    !    enddo
-    !enddo
 
     ! Equation:
     ! S(k) = Sum(over all atoms)[ f_i(k) * exp( 2*pi*i*k.r ) ]
@@ -100,21 +88,12 @@ program ft3d
             do k=1, nkz/2
                 dpz = (kminz+k*dkz)
                 kvec = sqrt(dpx**2+dpy**2+dpz**2)
-                !if(skgrid(nkx-i,nky-j,nkz-k) == 0.0) then
-                    !writetootherside = .true.
-                !else
-                    !writetootherside = .false.
-                !endif
                 do n=1, m%natoms
                     dp = dpx*m%xx%ind(n) + dpy*m%yy%ind(n) + dpz*m%zz%ind(n)
                     !sk = f_e(m%znum%ind(n),kvec) * cdexp(cpi2*dp)
                     sk = cdexp(cpi2*dp)
                     skgrid(i,j,k) = skgrid(i,j,k) + sk
-                    !if(writetootherside) then
-                    !if( i/=allbinsize/2+1 .or. j/=allbinsize/2+1 .or. k/=allbinsize/2+1) then
-                    !if( i/=allbinsize/2 .or. k/=allbinsize/2) then
-                        skgrid(nkx-i,nky-j,nkz-k) = skgrid(nkx-i,nky-j,nkz-k) + conjg(sk)
-                    !endif
+                    skgrid(nkx-i+1,nky-j+1,nkz-k+1) = skgrid(nkx-i+1,nky-j+1,nkz-k+1) + conjg(sk)
                 enddo
             enddo
         enddo
@@ -129,21 +108,20 @@ program ft3d
         do j=1, nky
             do k=1, nkz
                 ikgrid(i,j,k) = cdabs(skgrid(i,j,k))
-                !ikgrid(nkx-i,nky-j,nkz-k) = cdabs(conjg(skgrid(i,j,k)))
             enddo
         enddo
     enddo
 
     write(*,*) "Writing output..."
+    open(unit=52,file='Zr50_t3_256.gfx',form='formatted',status='unknown')
+    !open(unit=52,file='Zr50_t3_64.gfx',form='formatted',status='unknown')
     !open(unit=52,file='al_3x3x3.gfx',form='formatted',status='unknown')
     !open(unit=52,file='al_chunk.gfx',form='formatted',status='unknown')
-    !open(unit=52,file='Zr50_t3_256.gfx',form='formatted',status='unknown')
-    !open(unit=52,file='Zr50_t3_64.gfx',form='formatted',status='unknown')
     !open(unit=52,file='ZCA_t3_xtal.gfx',form='formatted',status='unknown')
     !open(unit=52,file='xtal.t3.opposite.gfx',form='formatted',status='unknown')
     !open(unit=52,file='icolike.t3.gfx',form='formatted',status='unknown')
     !open(unit=52,file='mixed.gfx',form='formatted',status='unknown')
-    open(unit=52,file='mixed_icolike.gfx',form='formatted',status='unknown')
+    !open(unit=52,file='mixed_icolike.gfx',form='formatted',status='unknown')
     do i=1, nkx
         do j=1, nky
             do k=1, nkz
@@ -153,7 +131,6 @@ program ft3d
         write(*,*) i*(100.0/nkx), "percent done"
     enddo
     close(52)
-    !call Write3DGFX('testing.gfx', ikgrid, istat)
     
 
 end program ft3d
